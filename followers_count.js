bc8b960f8201be4ca6fa5d60c5d9a279505a84e9
Siddhesh Singh <sidsinghrocks360@gmail.com>
2024-06-12 15:34:23 -0400

    Hands-on Lab: Develop Followers Count Milestone Alerts using Functions

---
+++ followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; //Initialize count to 0
+
+function IncreaseCount() {
+    count++; //Increment the count by 1
+    displayCount(); //Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count; // Display the counnt in the HTML
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram account gained 10 followers! Congratulations");
+    } else if (count === 20) {
+        alert("Your Instagram account gained 20 followers! Keep it up!");
+    }
+}
